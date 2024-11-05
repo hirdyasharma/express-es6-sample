d9615da3f8c2c0abfe14c8464ac080073c3c6929
Hirdya Sharma <53007013+hirdyasharma@users.noreply.github.com>
2024-11-05 15:46:05 +0800

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('My name is Hirdya Sharma, student ID is 21749180');
+  res.render('index', { title: 'World' });
 
 });
 
